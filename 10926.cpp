00a2fd61970235fe23e4dd291e2263bbf4298513
jihunhan98 <83203316+jihunhan98@users.noreply.github.com>
2022-02-23 16:33:36 +0900

    Create 10926.cpp

---
+++ 10926.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    string s;
+    cin >> s;
+    
+    s = s + "??!";
+    
+    cout << s << endl;
+    
+    return 0;
+}
+
+
