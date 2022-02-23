60a7bc8e4b99d7bafbac684ed0380ca756b14ec7
jihunhan98 <83203316+jihunhan98@users.noreply.github.com>
2022-02-23 16:36:13 +0900

    Create 18108.cpp

---
+++ 18108.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int y;
+    cin >> y;
+    
+    cout << y - 543 << endl;
+    return 0;
+    
+}
+
+
